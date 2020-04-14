d5593f852702ce19eb6e6baa34680f8c9e528cc8
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-04-14 20:50:56 +0200

    Updates

---
+++ precache-manifest.cd4792b5d5e6a1c36f9d35b5847b8dbf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2d9ce18c9594232c5bb7fb684f8df0",
+    "url": "/NIMA/index.html"
+  },
+  {
+    "revision": "fc9c2b027aed7aefbb02",
+    "url": "/NIMA/static/css/2.c68737a4.chunk.css"
+  },
+  {
+    "revision": "5b65353812576becb58d",
+    "url": "/NIMA/static/css/main.deac5db1.chunk.css"
+  },
+  {
+    "revision": "fc9c2b027aed7aefbb02",
+    "url": "/NIMA/static/js/2.792b6072.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/NIMA/static/js/2.792b6072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b65353812576becb58d",
+    "url": "/NIMA/static/js/main.1f2a8110.chunk.js"
+  },
+  {
+    "revision": "e4f8d802a0436cc1a595",
+    "url": "/NIMA/static/js/runtime-main.b6f23ac4.js"
+  },
+  {
+    "revision": "c05f86a2dc9024db20b9e2cf1290673b",
+    "url": "/NIMA/static/media/blueprint.c05f86a2.jpg"
+  },
+  {
+    "revision": "a8870be8da568a4adcda0f3ce48f4b59",
+    "url": "/NIMA/static/media/cover1.a8870be8.jpg"
+  },
+  {
+    "revision": "61db4097166d2b19ad9ff52a7363870d",
+    "url": "/NIMA/static/media/hus1.61db4097.jpg"
+  },
+  {
+    "revision": "07635fc33a728c9e5687f0d2d8119624",
+    "url": "/NIMA/static/media/hus1_tumb.07635fc3.jpg"
+  },
+  {
+    "revision": "21b16508d1c5ec5418d7596b3fb3532e",
+    "url": "/NIMA/static/media/hus2.21b16508.jpg"
+  },
+  {
+    "revision": "716e694a3d4e8c9e7f9fb291aaf86271",
+    "url": "/NIMA/static/media/hus2_tumb.716e694a.jpg"
+  },
+  {
+    "revision": "cf1143326078ee0a63a0a8ed59ae28fc",
+    "url": "/NIMA/static/media/hus3.cf114332.jpg"
+  },
+  {
+    "revision": "1fd41c4c3adf16d0dca5d8ce0424214e",
+    "url": "/NIMA/static/media/hus3_tumb.1fd41c4c.jpg"
+  },
+  {
+    "revision": "aea321146c50b460e48b39006eac893d",
+    "url": "/NIMA/static/media/hus4.aea32114.jpg"
+  },
+  {
+    "revision": "e383fd89fa6b19d35f51908618307fa5",
+    "url": "/NIMA/static/media/hus4_tumb.e383fd89.jpg"
+  },
+  {
+    "revision": "9d27fb7fe3eb9ce84cacdcf604b07f49",
+    "url": "/NIMA/static/media/hus5.9d27fb7f.jpg"
+  },
+  {
+    "revision": "d341a71447c839697f8671cc2fd9e5d6",
+    "url": "/NIMA/static/media/hus5_tumb.d341a714.jpg"
+  },
+  {
+    "revision": "de4c40b9dce612d617c51ce0a4389026",
+    "url": "/NIMA/static/media/logo_svg.de4c40b9.svg"
+  },
+  {
+    "revision": "1331d2e70c339d45825c2fe4344ad360",
+    "url": "/NIMA/static/media/placeholder.1331d2e7.jpg"
+  }
+]);
